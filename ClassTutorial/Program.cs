376fc20ede57b7bfc09f11defd6e9265105d67a1
Elyse Roth <elysemorganroth@gmail.com>
2021-09-27 14:12:50 -0400

    Add project files.

---
+++ ClassTutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassTutorial
+{
+    class Program //this is a class that already exists, can create more than one class in a file
+    {
+        static void Main(string[] args)
+        {//template for what "students" are going to look like: here it's name property and print method
+            var fred = new Student(); //create instance with new + name of class + () + ;
+            //set name to value (only property in class)
+            fred.Name = "Fred"; //var that points to instance + property bc could be other student instances
+            fred.SAT = 1000;
+            var wilma = new Student(); //creating a new student instance without any data
+            //**Each piece of data must be unique every piece of data needs its own var**
+            wilma.Name = "Wilma";
+            //must start with var to differentiate student
+            wilma.SAT = 2000;
+            wilma.Print();
+            fred.Print();
+            //several instances of same class within all different variables bc always have to point to the
+            //instance/var you're looking to act on
+        }
+    } // new class must be after the closing } for the previous class, don't put inside of the other class if putting in same window
+
+}
---
+++ ClassTutorial/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTutorial
+{
+    class Student
+    {
+        public string Name { get; set; } //public (access), string (type), Name (name)
+                                         //if public, get and set are public (anyone can read, edit)
+                                         //most properties will look like the name line
+        private int _SAT = 800; //no other class can access it, use _ for internal variables only, where data stored
+        public int SAT
+        {  //make it so anyone can read value but if edit ensure can only be between 400 and 1600
+            get { return _SAT; } //value held in _SAT, get/set will allow user to interact with it
+            set
+            {               //checks value they try to set it to before put it in
+
+                if (value < 400 || value > 1600)
+                {
+                    Console.WriteLine("Invalid SAT score.");
+                }
+                else
+                {
+                    this._SAT = value;
+                }
+            }
+        }
+
+        public void Print() //can pass parameters in here if need/want
+        {
+            Console.WriteLine($"name:{this.Name} sat:{this.SAT}"); //will print whatever we put in as name for student
+                                          //when inside your class and referencing property from inside, use this. to reference what accessing
+                                          //usually "this" is optional as long as they know what you're referring to (usually fine unless duplicates)
+        }
+    }
+}
